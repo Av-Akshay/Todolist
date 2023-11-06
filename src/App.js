52d897f81b14ba4e30bfd42913d33b6a568e9717
Anurag <anuragchauhan7248@gmail.com>
2023-11-06 12:12:18 +0530

    fix: problem occer when add a item

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import React, { useEffect, useState } from "react";
-import List from "./List";
+import React, { useEffect, useState } from 'react'
+import List from './List'
 
 function App() {
-  const [item, setitem] = useState("");
-  const [additem, setadditem] = useState([]);
+  const [item, setitem] = useState('')
+  const [additem, setadditem] = useState([])
 
   const inputchange = (event) => {
-    setitem(event.target.value);
-  };
+    setitem(event.target.value)
+  }
   useEffect(() => {
-    let listData = JSON.parse(localStorage.getItem("list"));
-    setadditem(listData);
-  }, []);
+    let listData = JSON.parse(localStorage.getItem('list'))
+    setadditem(listData)
+  }, [])
   useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(additem));
-  }, [additem]);
+    localStorage.setItem('list', JSON.stringify(additem))
+  }, [additem])
 
   const Addlist = () => {
     setadditem((olditem) => {
-      if (item === "") {
-        return [...olditem];
+      if (item.length === 0) {
+        alert('Input fild is Empty')
+        return [...olditem]
       } else if (item.length <= 2) {
-        alert("Minumum three letters required");
-        return [...olditem];
-      } else {
-        return [...olditem, item];
+        alert('Minumum three letters required')
+        return [...olditem]
+      } else if (item.length > 2) {
+        if (olditem === null) {
+          return [item]
+        } else {
+          console.log(olditem)
+          return [...olditem, item]
+        }
       }
-    });
-    setitem("");
-  };
+    })
+    setitem('')
+    console.log(additem)
+  }
 
   const deletitem = (id) => {
     setadditem((olditem) => {
       return olditem.filter((array, index) => {
-        return index !== id;
-      });
-    });
-  };
+        return index !== id
+      })
+    })
+  }
 
   return (
     <div className="box">
@@ -62,12 +69,12 @@ function App() {
                 id={index}
                 ondelete={deletitem}
               />
-            );
+            )
           })}
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
